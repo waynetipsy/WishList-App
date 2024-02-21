ad59bd46189e75c5f4aad5db832dd31ca8925ab7
waynetipsy <jacketoniru@gmail.com>
2024-02-21 14:48:50 +0100

    UI/Navigation

---
+++ app/src/main/java/app/ify/mywishlistapp/HomeView.kt
@@ -0,0 +1,33 @@
+package app.ify.mywishlistapp
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun HomeView() {
+    val context = LocalContext.current
+  Scaffold (
+    topBar = { AppBarView(title = "WishList", {
+        Toast.makeText(context, "Button Clicked", Toast.LENGTH_LONG).show()
+    })},
+      floatingActionButton = {
+          FloatingActionButton(onClick = { // Add Navigation to add screen }) {
+              
+          }
+      }
+  ){
+   LazyColumn(modifier = Modifier
+       .fillMaxSize()
+       .padding(it)){
+
+
+   }
+  }
+}
