9f0b77b7a3a209fa37491356547503e01de38fbc
waynetipsy <jacketoniru@gmail.com>
2024-04-11 01:50:30 +0100

    new updates

--- app/src/main/java/app/ify/mywishlistapp/HomeView.kt
+++ app/src/main/java/app/ify/mywishlistapp/HomeView.kt
@@ -1,7 +1,10 @@
 package app.ify.mywishlistapp
 
 import android.widget.Toast
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -9,16 +12,25 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.DismissDirection
+import androidx.compose.material3.DismissState
+import androidx.compose.material3.DismissValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SwipeToDismiss
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDismissState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -28,6 +40,7 @@ import androidx.navigation.NavController
 import app.ify.mywishlistapp.data.DummyWish
 import app.ify.mywishlistapp.data.Wish
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeView(
     navController: NavController,
@@ -59,16 +72,55 @@ fun HomeView(
        .fillMaxSize()
        .padding(it)){
       items(wishlist.value) {
-          wish -> WishItem(wish = wish) {
-              val id = wish.id
-          navController.navigate(Screen.AddScreen.route + "/$id")
+          wish ->
+          val dismissState = rememberDismissState(
+              confirmValueChange = {
+                  if (it == DismissValue.DismissedToEnd || it== DismissValue.DismissedToStart){
+                      viewModel.deleteWish(wish)
+                  }
+                  true
+              }
+          )
+
+          SwipeToDismiss(
+              state = dismissState,
+              background = {
+                  val color by animateColorAsState(
+                      if(dismissState.dismissDirection
+                          == DismissDirection.EndToStart) Color.Red else Color.Transparent
+                      , label = ""
+                  )
+                  val alignment = Alignment.CenterEnd
+                  Box(
+                      Modifier
+                          .fillMaxSize()
+                          .background(color)
+                          .padding(horizontal = 20.dp),
+                      contentAlignment = alignment
+                      ){
+                      Icon(Icons.Default.Delete,
+                          contentDescription = "Delete Icon",
+                          tint = Color.White
+
+                          )
+
+                  }
+              },
+              directions = setOf( DismissDirection.EndToStart),
+              dismissContent = {
+                  WishItem(wish = wish) {
+                      val id = wish.id
+                      navController.navigate(Screen.AddScreen.route + "/$id")
+                  }
+              }
+          )
+
 
       }
       }
 
    }
   }
-}
 
 @Composable
 fun WishItem(wish: Wish, onClick: () -> Unit){
